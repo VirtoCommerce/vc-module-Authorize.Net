89d1df054f5991c7725be3b65ae2f664b46f16bb
Andrey Kozlov <yecli.github@gmail.com>
2020-02-03 22:32:30 +0500

    VP-1310: Migrate to V3
    - Structure changed;
    - Adopted code;
    - Settings addition in process;

---
+++ src/VirtoCommerce.AuthorizeNet.Web/Module.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using VirtoCommerce.AuthorizeNet.Web.Managers;
+using VirtoCommerce.PaymentModule.Core.Services;
+using VirtoCommerce.Platform.Core.Modularity;
+using VirtoCommerce.Platform.Core.Settings;
+
+namespace VirtoCommerce.AuthorizeNet.Web
+{
+	public class Module : IModule
+	{
+		public ManifestModuleInfo ModuleInfo { get; set; }
+
+		#region IModule Members
+
+		public void Initialize(IServiceCollection serviceCollection)
+		{
+			// No need in actions
+		}
+
+		public void PostInitialize(IApplicationBuilder appBuilder)
+		{
+			var settingsRegistrar = appBuilder.ApplicationServices.GetRequiredService<ISettingsRegistrar>();
+			settingsRegistrar.RegisterSettings(ModuleConstants.Settings.AllSettings, ModuleInfo.Id);
+			settingsRegistrar.RegisterSettingsForType(ModuleConstants.Settings.AuthorizeNet.Settings, nameof(AuthorizeNetMethod));
+
+			var settingsManager = appBuilder.ApplicationServices.GetRequiredService<ISettingsManager>();
+
+			Func<AuthorizeNetMethod> authorizeNetPaymentMethodFactory = () =>
+			{
+				var result = new AuthorizeNetMethod
+				{
+					LogoUrl = "https://raw.githubusercontent.com/VirtoCommerce/vc-module-Authorize.Net/master/Authorize.Net/Content/Authorizenet_logo.png",
+					IsActive = false,
+				};
+				var settingNames = ModuleConstants.Settings.AuthorizeNet.Settings.Select(x => x.Name);
+				var settings = settingsManager.GetObjectSettingsAsync(settingNames, result.TypeName, result.Id).GetAwaiter().GetResult().ToList();
+
+				result.Settings = settings;
+
+				return result;
+			};
+
+			var paymentMethodsRegistrar = appBuilder.ApplicationServices.GetRequiredService<IPaymentMethodsRegistrar>();
+			paymentMethodsRegistrar.RegisterPaymentMethod(authorizeNetPaymentMethodFactory);
+		}
+
+		public void Uninstall()
+		{
+			// No need in actions
+		}
+		#endregion
+	}
+}
---
+++ src/VirtoCommerce.AuthorizeNet.Web/ModuleConstants.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using VirtoCommerce.Platform.Core.Settings;
+
+namespace VirtoCommerce.AuthorizeNet.Web
+{
+	public static class ModuleConstants
+	{
+		public static class Settings
+		{
+			public static class AuthorizeNet
+			{
+				public static readonly SettingDescriptor ApiLogin = new SettingDescriptor
+				{
+					Name = "VirtoCommerce.Payment.AuthorizeNet.ApiLogin",
+					GroupName = "Payment|Authorize.Net",
+					ValueType = SettingValueType.ShortText,
+					IsDictionary = false
+				};
+
+				public static IEnumerable<SettingDescriptor> Settings
+				{
+					get
+					{
+						return new List<SettingDescriptor>
+						{
+							ApiLogin,
+						};
+					}
+				}
+			}
+
+			public static IEnumerable<SettingDescriptor> AllSettings
+			{
+				get
+				{
+					return AuthorizeNet.Settings;
+				}
+			}
+		}
+	}
+}
