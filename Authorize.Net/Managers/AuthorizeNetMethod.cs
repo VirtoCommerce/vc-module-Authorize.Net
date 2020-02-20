2849e46342ae8abe8906c7e78d0ac6e724c2dd34
Andrey Kostyrin <kostyrin2@gmail.com>
2020-02-20 16:07:55 +0200

    #VP-1475 make refund (#7)

--- Authorize.Net/Managers/AuthorizeNetMethod.cs
+++ Authorize.Net/Managers/AuthorizeNetMethod.cs
@@ -8,6 +8,7 @@ using System.Text;
 using AuthorizeNet;
 using VirtoCommerce.Domain.Order.Model;
 using VirtoCommerce.Domain.Payment.Model;
+using VirtoCommerce.Platform.Core.Common;
 
 namespace Authorize.Net.Managers
 {
@@ -257,7 +258,14 @@ namespace Authorize.Net.Managers
 
         public override RefundProcessPaymentResult RefundProcessPayment(RefundProcessPaymentEvaluationContext context)
         {
-            throw new NotImplementedException();
+            var refundStatus = new RefundProcessPaymentResult { IsSuccess = true, ErrorMessage = "" };
+            if (context.Payment.IsApproved && context.Payment.PaymentStatus == PaymentStatus.Paid)
+            {
+                context.Payment.PaymentStatus = refundStatus.NewPaymentStatus = PaymentStatus.Refunded;
+                context.Payment.Status = PaymentStatus.Refunded.ToString();
+                context.Payment.VoidedDate = DateTime.UtcNow;
+            }
+            return refundStatus;
         }
 
         public override ValidatePostProcessRequestResult ValidatePostProcessRequest(NameValueCollection queryString)
@@ -305,6 +313,7 @@ namespace Authorize.Net.Managers
                     context.Payment.IsCancelled = true;
                     retVal.IsSuccess = true;
                     retVal.NewPaymentStatus = context.Payment.PaymentStatus = PaymentStatus.Voided;
+                    context.Payment.Status = PaymentStatus.Voided.ToString();
                     context.Payment.VoidedDate = context.Payment.CancelledDate = DateTime.UtcNow;
                 }
                 else
