19812f33bdc6e4ad15d819c1f2a3a4171faf49e9
Andrey Kostyrin <kostyrin2@gmail.com>
2020-02-20 16:07:32 +0200

    #VP-1423 load Customer IP address (#6)

--- Authorize.Net/Managers/AuthorizeNetMethod.cs
+++ Authorize.Net/Managers/AuthorizeNetMethod.cs
@@ -238,6 +238,7 @@ namespace Authorize.Net.Managers
                 checkoutform += CreateInput(true, "x_fp_hash", fingerprint);
                 checkoutform += CreateInput(true, "x_currency_code", currency);
                 checkoutform += CreateInput(true, "x_amount", context.Payment.Sum.ToString("F", CultureInfo.InvariantCulture));
+                checkoutform += CreateInput(true, "x_customer_ip", GetIPAddress());
 
                 checkoutform += GetAuthOrCapture();
 
@@ -419,6 +420,23 @@ namespace Authorize.Net.Managers
             }
 
             return retVal;
+        }
+
+        private string GetIPAddress()
+        {
+            System.Web.HttpContext context = System.Web.HttpContext.Current;
+            string ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                string[] addresses = ipAddress.Split(',');
+                if (addresses.Length != 0)
+                {
+                    return addresses[0];
+                }
+            }
+
+            return context.Request.ServerVariables["REMOTE_ADDR"];
         }
     }
 }
