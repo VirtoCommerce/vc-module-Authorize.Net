8640e3aeff40ee765cb2ab1870a38381ce975b45
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-01-31 11:05:02 +0200

    Add tests project

---
+++ tests/VirtoCommerce.AuthorizeNet.Tests/Class1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace VirtoCommerce.AuthorizeNet.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
