fe8b1d8f92a888d0f09e1f2be56bc75edf6df925
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-01-31 14:15:51 +0200

    Add comment about the useless test

--- tests/VirtoCommerce.AuthorizeNet.Tests/Class1.cs
+++ tests/VirtoCommerce.AuthorizeNet.Tests/Class1.cs
@@ -4,6 +4,10 @@ namespace VirtoCommerce.AuthorizeNet.Tests
 {
     public class Class1
     {
+        /// <summary>
+        /// We need at least one test, to make CI works :)
+        /// Reminder: Remove if useful tests are appeared 
+        /// </summary>
         [Fact]
         public void Test()
         {
